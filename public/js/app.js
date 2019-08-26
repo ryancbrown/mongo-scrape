6c9423b5bd66eec977edf926eceda77a1fb650ed
rcbnyc <16548379+ryancbrown@users.noreply.github.com>
2019-08-26 18:21:30 -0400

    update old messaging around delete message from 'y' to 'x'

--- public/js/app.js
+++ public/js/app.js
@@ -58,7 +58,7 @@ $(document).on("click", ".note", function(e) {
     // Post to server for it to be saved
     $.post("/note/save", note, (e) => {
         count = e.note.length
-        $(`#note-${id}`).append(`<li class="bg-light pr-3 rounded" data-note=${e.note[count-1]}><b>${moment().format("YYYY-MM-DD")}</b><br> ${noteText.val()}<div class="delete bg-danger d-none rounded-right px-3"><b>Y</b></div></li>`)
+        $(`#note-${id}`).append(`<li class="bg-light pr-3 rounded" data-note=${e.note[count-1]}><b>${moment().format("YYYY-MM-DD")}</b><br> ${noteText.val()}<div class="delete bg-danger d-none rounded-right px-3"><b>X</b></div></li>`)
         $(document).find(`[data-article='${id}']`).text(`Notes (${count})`)
         noteText.val("")
     });   
@@ -75,7 +75,7 @@ $(document).on("click", ".note-button", function(e) {
 
         $.each(e, (i, element) => { 
             // Append notes
-            $(`#note-${id}`).append(`<li class="bg-light pr-3 rounded-left" data-note=${e[i]._id}><b>${moment(e[i].date).format("YYYY-MM-DD")}</b><br> ${e[i].body}<div class="delete bg-danger d-none rounded-right px-3"><b>Y</b></div></li>`)
+            $(`#note-${id}`).append(`<li class="bg-light pr-3 rounded-left" data-note=${e[i]._id}><b>${moment(e[i].date).format("YYYY-MM-DD")}</b><br> ${e[i].body}<div class="delete bg-danger d-none rounded-right px-3"><b>X</b></div></li>`)
         });
     }); 
     // Start at the anchor, move up to the parent div
